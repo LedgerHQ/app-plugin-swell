dfd45aa22b2bab719dc528432c0db431c7be98ff
Francois Beutin <francois.beutin@ledger.fr>
2025-09-10 11:46:14 +0200

    Add support for APEX_P

--- tests/tests/test_rsweth_token.py
+++ tests/tests/test_rsweth_token.py
@@ -5,13 +5,12 @@ contract_rsweth_token = load_contract(
     "fae103dc9cf190ed75350761e95403b7b8afa6c0"
 )
 
-def test_rsweth_token_deposit(backend, firmware, navigator, test_name, wallet_addr):
+def test_rsweth_token_deposit(backend, navigator, test_name, wallet_addr):
     data = "0xd0e30db0"
     run_test(
         contract_rsweth_token, 
         data, 
         backend, 
-        firmware, 
         navigator, 
         test_name, 
         wallet_addr
